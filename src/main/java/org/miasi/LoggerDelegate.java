ee97a1d00032c5f7a07bb1e9d4061ef238afcecb
karololeksy <karol.oleksy97@gmail.com>
2020-05-26 18:57:25 +0200

    Init

--- src/main/java/org/miasi/LoggerDelegate.java
+++ src/main/java/org/miasi/LoggerDelegate.java
@@ -1,15 +1,18 @@
-package com.miasi;
+package org.miasi;
 
 import java.util.logging.Logger;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
 
 /**
  * This is an easy adapter implementation 
  * illustrating how a Java Delegate can be used 
  * from within a BPMN 2.0 Service Task.
  */
+@Component("logger")
 public class LoggerDelegate implements JavaDelegate {
  
   private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
@@ -24,7 +27,6 @@ public class LoggerDelegate implements JavaDelegate {
             + ", businessKey=" + execution.getProcessBusinessKey()
             + ", executionId=" + execution.getId()
             + " \n\n");
-    
   }
 
 }
